b730a7cb24b942bfe7db2a9ad44d54a790e8f893
马博渊 <793960628@qq.com>
2018-05-28 13:40:11 +0800

    5-28

--- stitchMain.py
+++ stitchMain.py
@@ -21,11 +21,11 @@ def stitchWithFeatureIncre():
     # stitcher.imageSetStitchWithMutiple(projectAddress, outputAddress, 50, stitcher.calculateOffsetForFeatureSearchIncre,
     #                         startNum=1, fileExtension="jpg", outputfileExtension="jpg")
 
-    # method = "featureSearchIncre"; Stitcher.direction = 1;  Stitcher.directIncre = 1;
-    # projectAddress = "images\\dendriticCrystal"
-    # outputAddress = "result\\" + method + "\\dendriticCrystal" + str.capitalize(Stitcher.fuseMethod) + "\\"
-    # stitcher.imageSetStitchWithMutiple(projectAddress, outputAddress, 11, stitcher.calculateOffsetForFeatureSearchIncre,
-    #                         startNum=3, fileExtension="jpg", outputfileExtension="jpg")
+    method = "featureSearchIncre"; Stitcher.direction = 1;  Stitcher.directIncre = 1;
+    projectAddress = "images\\dendriticCrystal"
+    outputAddress = "result\\" + method + "\\dendriticCrystal" + str.capitalize(Stitcher.fuseMethod) + "\\"
+    stitcher.imageSetStitchWithMutiple(projectAddress, outputAddress, 11, stitcher.calculateOffsetForFeatureSearchIncre,
+                            startNum=3, fileExtension="jpg", outputfileExtension="jpg")
 
     # Stitcher.featureMethod = "surf"; Stitcher.searchRatio = 0.95; Stitcher.offsetEvaluate = 3;
     # method = "featureSearchIncre";  Stitcher.direction = 1;  Stitcher.directIncre = 1;
