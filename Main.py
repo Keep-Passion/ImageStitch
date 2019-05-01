99e9feb4747cff0a7f720050546d21b3353810a6
马博渊 <793960628@qq.com>
2019-05-01 11:47:39 +0800

    update readme

--- Main.py
+++ Main.py
@@ -50,18 +50,3 @@ def stitchWithFeature():
 
 if __name__=="__main__":
     stitchWithFeature()
-    # Stitcher.featureMethod = "surf"  # "sift","surf" or "orb"
-    # Stitcher.isGPUAvailable = True
-    # Stitcher.searchRatio = 0.75  # 0.75 is common value for matches
-    # Stitcher.offsetCaculate = "mode"  # "mode" or "ransac"
-    # Stitcher.offsetEvaluate = 3  # 3 menas nums of matches for mode, 3.0 menas  of matches for ransac
-    # Stitcher.roiRatio = 0.2  # roi length for stitching in first direction
-    # Stitcher.fuseMethod = "trigonometric"  # "notFuse","average","maximum","minimum","fadeInAndFadeOut","trigonometric", "multiBandBlending"
-    # stitcher = Stitcher()
-    #
-    # Stitcher.direction = 1;
-    # Stitcher.directIncre = -1;
-    # projectAddress = "demoImages\\other"
-    # outputAddress = "result\\other" + str.capitalize(Stitcher.fuseMethod) + "\\"
-    # stitcher.imageSetStitchWithMutiple(projectAddress, outputAddress, 1, stitcher.calculateOffsetForFeatureSearchIncre,
-    #                                    startNum=1, fileExtension="bmp", outputfileExtension="jpg")
